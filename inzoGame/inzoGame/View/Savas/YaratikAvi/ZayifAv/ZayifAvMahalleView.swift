ea5757196dc7913c9ad430e32c2d10aa77526224
yunus gündüz <yns3gndz@hotmail.com.tr>
2022-09-11 11:48:19 +0300

    mahallede av kodları sadece veri çekiyor , hesaplamalar yapılacak

--- inzoGame/inzoGame/View/Savas/YaratikAvi/ZayifAv/ZayifAvMahalleView.swift
+++ inzoGame/inzoGame/View/Savas/YaratikAvi/ZayifAv/ZayifAvMahalleView.swift
@@ -13,15 +13,25 @@ struct ZayifAvMahalleView: View {
     var gelenAvMaxLevel:Int?
     
     let userDefaults = UserDefaults.standard
-    @State var username:String=""
+    @State var username:String?
     
-    @State    var userLevel:String = " "
-    @State   var userBattleValue:String = " "
-    @State   var userGold:String = " "
-    @State  var userExp:String = " "
-    @State    var userHpNow:String = " "
-    @State    var userApPow:String = " "
-    @State   var userApDef:String = " "
+    @State     var userLevel:String?
+    @State    var userBattleValue:String?
+    @State    var userGold:String?
+    @State  var userExp:String?
+    @State    var userHpNow:String?
+    @State    var userApPow:String?
+    @State   var userApDef:String?
+    
+    
+    @State   var monsterName:String?
+    @State    var monsterLevel:String?
+    @State  var monsterGold:String?
+    @State  var monsterPow:String?
+    @State  var monsterDef:String?
+    
+   
+    
     
     
     var body: some View {
@@ -32,17 +42,42 @@ struct ZayifAvMahalleView: View {
           Text("").onAppear {
               veriCekmeMain()
               veriCekmeMonster()
-           
       }
-            let userLevelCasting = Int(userLevel) ?? -9999
-            
+            let userLevelCasting = Int(userLevel ?? "0") ?? -9999
             
             if userLevelCasting == -9999 {
                 Text("sonuclar yukleniyor").padding()
             }
             
             else {
-                Text("Rakip  cikti \(gelenAvMinLevel!) ,  \(gelenAvMaxLevel!)").padding()
+                VStack{
+                    HStack{
+                        
+                        Text("Avlanma Raporu").padding(10).foregroundColor(Color.red)
+                    }
+                   
+                    VStack{
+                        
+                        Text("Saldıran: \(username ?? "none")  Savunan: \(monsterName ?? "none") ").padding()
+                        Text("Saldıran lv: \(userLevel ?? "none")   Savunan lv: \(monsterLevel ?? "none") ").padding()
+                        Text("Saldıran gold: \(userGold ?? "none")  Savunan gold: \(monsterGold ?? "none") ").padding()
+                        Text("Saldıran Güç: \(userApPow ?? "none")  Savunan Güç: \(monsterPow ?? "none") ").padding()
+                        Text("Saldıran Defans: \(userApDef ?? "none")    Savunan Defans: \(monsterDef ?? "none" ) ").padding(20)
+                        
+                     
+                        Text("Av sonucu kalan can: \(userHpNow ?? "none")")
+                        Text("Av sonucu exp:  \(userExp ?? "none")").padding()
+                       
+                        
+                        
+                        
+                        
+                        
+                    }
+                }
+               
+                
+                
             }
             
             Button("TAMAM"){
@@ -55,8 +90,8 @@ struct ZayifAvMahalleView: View {
     }
     fileprivate func veriCekmeMonster() {
      
-        print("max: \(gelenAvMaxLevel)")
-        print("min: \(gelenAvMinLevel)")
+        print("max: \(gelenAvMaxLevel!)")
+        print("min: \(gelenAvMinLevel!)")
         let url = URL(string: "http://yunusgunduz.site/inzoApi/public/api/monster?max=\(gelenAvMaxLevel!)&min=\(gelenAvMinLevel!)")
         let session  = URLSession.shared
         let task = session.dataTask(with: url!) { data, response, error in
@@ -72,12 +107,14 @@ struct ZayifAvMahalleView: View {
                             print(jsonResponse!["name"]!)
                             
                             
-                            //kasitli username yazmadim
-                            
-                          //   userLevel = jsonResponse!["level"]! as! String
                             
                             
                             
+                            monsterName = jsonResponse!["name"]! as? String
+                            monsterLevel = (jsonResponse!["lvl"]! as! String)
+                            monsterGold = (jsonResponse!["cost_gold"]! as! String)
+                            monsterPow = (jsonResponse!["pow"]! as! String)
+                            monsterDef = (jsonResponse!["def"]! as! String)
                         }
                     }catch{
                         print(error)
@@ -91,7 +128,7 @@ struct ZayifAvMahalleView: View {
     fileprivate func veriCekmeMain() {
         username = userDefaults.object(forKey: "username")! as! String
         
-        let url = URL(string: "http://yunusgunduz.site/inzoApi/public/api/\(username)")
+        let url = URL(string: "http://yunusgunduz.site/inzoApi/public/api/\(username ?? "admin")")
         let session  = URLSession.shared
         let task = session.dataTask(with: url!) { data, response, error in
             if error != nil {
@@ -106,9 +143,16 @@ struct ZayifAvMahalleView: View {
                             print(jsonResponse!["level"]!)
                             
                             
-                            //kasitli username yazmadim
+                           
                             
-                             userLevel = jsonResponse!["level"]! as! String
+                             userLevel = jsonResponse!["level"] as! String ?? "0"
+                            userBattleValue = jsonResponse!["battle_value"]! as! String ?? "0"
+                            userGold = jsonResponse!["gold"]! as! String ?? "0"
+                            userExp = jsonResponse!["exp"]! as! String ?? "0"
+                            userHpNow = jsonResponse!["hp_now"]! as! String ?? "0"
+                            userApPow = jsonResponse!["ab_pow"]! as! String ?? "0"
+                            userApDef = jsonResponse!["ab_def"]! as! String ?? "0"
+                           
                             
                             
                             
